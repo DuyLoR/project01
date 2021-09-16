f3e62e1d2715eef22efbc3a126783ffb5c62327f
DuyLoR <duya7k50@gmail.com>
2021-09-16 20:50:28 +0700

    Create new web

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Wow! Food online</title>
+</head>
+
+<body>
+    <!-- start heading -->
+    <div id="heading">
+        <div class="container">
+            <div class="logo">
+                <img src="../project01/image/logo/logo.png" alt="logo">
+            </div>
+            <div class="nav-menu">
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Foods</a>
+                    </li>
+                    <li>
+                        <a href="">About</a>
+                    </li>
+                    <li>
+                        <a href="">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- end heading -->
+
+    <!-- start slider -->
+    <div id="slider">
+        <div class="container">
+            slider
+            <form action="">
+                <input type="search" name="search" placeholder="Search for food..">
+                <input type="submit" name="submit" value="Search">
+            </form>
+        </div>
+    </div>
+    <!-- end slider -->
+
+    <!-- start content -->
+    <div id="content">
+        <div class="container">
+            <h2>Lists of Foods</h2>
+
+            <div class="box-3">
+                box1
+            </div>
+
+            <div class="box-3">
+                box2
+            </div>
+
+            <div class="box-3">
+                box3
+            </div>
+
+        </div>
+    </div>
+    <!-- end content -->
+
+    <!-- start footer -->
+    <div id="footer">
+        <div class="container">
+            footet
+        </div>
+    </div>
+    <!-- end footer -->
+
+</body>
+
+</html>
